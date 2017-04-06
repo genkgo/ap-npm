13fa1f096a86e86938bd831ce7072efd0b2dd9d8
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-06 13:15:52 +0200

    Initial commit for router.js

--- src/router.js
+++ src/router.js
@@ -1,3 +1,7 @@
+import Express from 'express';
+
+let app = new Express();
+
 app.get('/-/package/:package/dist-tags', function () {
   container.get('route-dist-tags').handle(request, response);
 });
