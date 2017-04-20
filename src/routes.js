e4fbd926e5858dd32c7e464f4f7cfb91db96a288
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-20 14:51:03 +0200

    Add function to router to rewrite url params to req.params

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,6 @@ export default function (app, container) {
   app.use(logger);
   app.use(bodyParser.json({ strict: false, limit: '10mb' }));
 
-
   // Rewrite the requested url when a scoped package is used
   app.use(function(req, res, next) {
     let url = decodeURIComponent(req.url);
@@ -22,6 +21,19 @@ export default function (app, container) {
     }
     next();
   });
+
+  // Rewrite ? params to req.params and convert url into regular url
+  app.use(function(req, res, next) {
+    let url = decodeURIComponent(req.url);
+    let params = url.split('?');
+    for (let i = 1; i < params.length; i++) {
+      let param = params[i].split('=');
+      req.params[param[0]] = param[1];
+    }
+    req.url = params[0];
+    next();
+  });
+
 
   // *** AUTH ***
   app.put('/-/user/org.couchdb.user:_rev?/:revision?', function(req, res, next) {
@@ -67,6 +79,7 @@ export default function (app, container) {
   // *** PUBLISH ***
   // TODO: We have to route the ?write=true route seperately for support for `npm deprecate` && `npm unpublish`
   app.put('/:package/:_rev?/:revision?', access.can('publish'), function(req, res, next) {
+    console.log(req.headers);
     let route = container.get('route-package-canPublish');
     route.process(req, res);
   });
