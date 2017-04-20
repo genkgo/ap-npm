0172df145be7908284796c1195fff117ec5915ac
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-20 10:54:49 +0200

    route-auth-user-logout: Use an auth function instead of a local function

--- src/routes/auth-user-logout.js
+++ src/routes/auth-user-logout.js
@@ -8,27 +8,11 @@ export default class {
 
   process(httpRequest, httpResponse) {
     let token = httpRequest.headers.authorization.substr(7);
-    this.logout(token);
+    this.auth.userLogout(token);
 
     httpResponse.status(200);
     httpResponse.send({
       ok: 'Logged out'
     });
-  }
-
-  logout(token) {
-    let tokenLocation = path.join(__dirname, '../..', 'db', 'user_tokens.json');
-    let allTokens;
-
-    try {
-      let tokenString = fs.readFileSync(tokenLocation, 'utf8');
-      allTokens = JSON.parse(tokenString);
-      delete allTokens[token];
-    } catch (e) {
-      // Just make the tokens_db an empty db.
-      allTokens = {};
-    }
-
-    fs.writeFileSync(tokenLocation, JSON.stringify(allTokens, null, 2));
   }
 }
