cd1ba0a2ed3aed5bbc4876d9147029701cb73ac9
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-20 10:57:25 +0200

    Routes: Removed unnecessary comments and imports, added a TODO for future functionality

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,4 @@
-import Cookies from 'cookies';
 import bodyParser from 'body-parser';
-import Error from 'http-errors';
-import Path from 'path';
 import Access from './auth/access';
 
 export default function (app, container) {
@@ -65,90 +62,12 @@ export default function (app, container) {
   });
 
 
-
   // *** PUBLISH ***
+  // TODO: We have to route the ?write=true route seperately for support for `npm deprecate` && `npm unpublish`
   app.put('/:package/:_rev?/:revision?', function(req, res, next) {
-    console.log(req);
     let route = container.get('route-package-publish');
     route.process(req, res);
   });
-
-  // Not implemented yuet
-  // app.put('/-/package/:package/dist-tags', function(req, res, next) {
-
-  // console.log('/-/package/:package/dist-tags', req.headers);
-
-  // can('publish'), media('application/json'), expect_json,
-  // function(req, res, next) {
-
-  // storage.replace_tags(req.params.package, req.body, function(err) {
-  //   if (err) return next(err)
-  //   res.status(201)
-  //   return next({ ok: 'tags updated' })
-  // })
-  // });
-
-  // app.put('/:package/:tag',
-  //   can('publish'), media('application/json'), tag_package_version)
-  //
-  // app.post('/-/package/:package/dist-tags/:tag',
-  //   can('publish'), media('application/json'), tag_package_version)
-  //
-  // app.put('/-/package/:package/dist-tags/:tag',
-  //   can('publish'), media('application/json'), tag_package_version)
-
-
-  // app.get('/-/user/:org_couchdb_user', function(req, res, next) {
-  //   console.log("get AUTH");
-  //   res.status(200);
-  //   res.send({
-  //     ok: 'you are authenticated as "' + req.remote_user.name + '"',
-  //   })
-  // });
-
-  // *** AUTH ***
-  // Add user OR login user
-
-  // app.put('/-/user/:org_couchdb_user/:_rev?/:revision?', function(req, res, next) {
-  //   var token = (req.body.name && req.body.password)
-  //     ? auth.aes_encrypt(req.body.name + ':' + req.body.password).toString('base64')
-  //     : undefined
-  //   if (req.remote_user.name != null) {
-  //     res.status(201)
-  //     return next({
-  //       ok: "you are authenticated as '" + req.remote_user.name + "'",
-  //       //token: auth.issue_token(req.remote_user),
-  //       token: token,
-  //     })
-  //   } else {
-  //     if (typeof(req.body.name) !== 'string' || typeof(req.body.password) !== 'string') {
-  //       if (typeof(req.body.password_sha)) {
-  //         return next( Error[422]('your npm version is outdated\nPlease update to npm@1.4.5 or greater.\nSee https://github.com/rlidwka/sinopia/issues/93 for details.') )
-  //       } else {
-  //         return next( Error[422]('user/password is not found in request (npm issue?)') )
-  //       }
-  //     }
-  //     auth.add_user(req.body.name, req.body.password, function(err, user) {
-  //       if (err) {
-  //         if (err.status >= 400 && err.status < 500) {
-  //           // With npm registering is the same as logging in,
-  //           // and npm accepts only an 409 error.
-  //           // So, changing status code here.
-  //           return next( Error[409](err.message) )
-  //         }
-  //         return next(err)
-  //       }
-  //
-  //       req.remote_user = user
-  //       res.status(201)
-  //       return next({
-  //         ok: "user '" + req.body.name + "' created",
-  //         //token: auth.issue_token(req.remote_user),
-  //         token: token,
-  //       })
-  //     })
-  //   }
-  // })
 
   return app
 }
