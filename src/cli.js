c6016f00e082b4416bfbd85d1176382537d0dfec
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-28 17:34:31 +0200

    Change -c to --config, commander is buggy with both -c and --config

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,25 @@
 import commander from 'commander';
 import containerInit from './init';
 import fs from 'fs';
+import path from 'path';
 
 commander
   .command('serve')
   .alias('s')
   .description('serve ap-npm')
-  .option('-c, --config', "config file to use")
+  .option('--config', "config file to use")
   .action(function(config) {
     let container;
-
     if (fs.existsSync(config)) {
       console.log("using config: " + config + '\n');
       container = containerInit(config);
-    } else {
+    }
+    else if (fs.existsSync(path.join(__dirname, '../', config))) {
+      let configLocation = path.join(__dirname, '../', config);
+      console.log("using config: " + configLocation + '\n');
+      container = containerInit(configLocation);
+    }
+    else {
       console.log("using default config\n");
       container = containerInit("../config.json");
     }
