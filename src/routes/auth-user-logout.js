d526725e3b2e3d3077bee223732d99bdeb41cd56
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-04-28 12:48:12 +0200

    Improve -h information

--- src/routes/auth-user-logout.js
+++ src/routes/auth-user-logout.js
@@ -1,13 +1,10 @@
-import fs from 'fs';
-import path from 'path';
-
 export default class {
   constructor(auth) {
     this.auth = auth;
   }
 
   process(httpRequest, httpResponse) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       let token = httpRequest.headers.authorization.substr(7);
       this.auth.userLogout(token);
 
