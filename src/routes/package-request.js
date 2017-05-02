ff90ad43b98559556d52cbb1c50b25ffb91c9ba5
Meir Noordermeer <meirnoordermeer@me.com>
2017-05-02 22:47:19 +0200

    Catch error of storage.getPackageData and just send json data on package-request

--- src/routes/package-request.js
+++ src/routes/package-request.js
@@ -12,16 +12,18 @@ export default class {
       this.storage.getPackageData({
         name: httpRequest.params.package,
         version: httpRequest.params.version,
-      }).then((packageJson) => {
-        if (packageJson) {
-          httpResponse.send(JSON.stringify(packageJson));
+      }).catch((err) => reject(err))
+        .then((packageJson) => {
+
+        if (typeof packageJson === 'object') {
+          httpResponse.send(packageJson);
           resolve();
         } else {
           reject("404, package not found")
         }
       });
     }).catch((err) => {
-      httpResponse.send(err);
+      httpResponse.send(err.toString());
     })
   }
 }
