5a8959c9ef2a31cce13fd73eaba57e19f48cb6b1
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-05-02 15:40:24 +0200

    Add set and init command to ap-npm

--- src/cli.js
+++ src/cli.js
@@ -7,27 +7,39 @@ commander
   .command('serve')
   .alias('s')
   .description('serve ap-npm')
-  .option('--config', "config file to use")
+  .option('-c, --config', "config file to use")
   .action(function(config) {
     let container;
+
     if (fs.existsSync(config)) {
       console.log("using config: " + config + '\n');
       container = containerInit(config);
-    }
-    else if (typeof config === 'string') {
-      if (fs.existsSync(path.join(__dirname, '../', config))) {
-        let configLocation = path.join(__dirname, '../', config);
-        console.log("using config: " + configLocation + '\n');
-        container = containerInit(configLocation);
-      }
-    }
-    else {
+    } else {
       console.log("using default config\n");
-      container = containerInit("../config.json");
+      let configLocation = path.join(__dirname, '../', 'config.json');
+      container = containerInit(configLocation);
     }
 
     let command = container.get('command-serve');
     command.run();
+  });
+
+commander
+  .command('set <prop> <value>')
+  .description('sets default config property')
+  .action(function(property, value) {
+    let container = containerInit(path.join(__dirname, '../', 'config.json'));
+    let command = container.get('command-set');
+    command.run(property, value);
+  });
+
+commander
+  .command('init')
+  .description('init a npm project using ap-npm config')
+  .action(function() {
+    let container = containerInit(path.join(__dirname, '../', 'config.json'));
+    let command = container.get('command-init');
+    command.run(process.cwd());
   });
 
 commander.parse(process.argv);
