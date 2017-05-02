3d48cdeb803e6b7d1320eb84c47060e8786a1436
Meir Noordermeer <meirnoordermeer@me.com>
2017-05-02 22:48:21 +0200

    Catch package-get errors

--- src/routes/package-get.js
+++ src/routes/package-get.js
@@ -9,12 +9,16 @@ export default class {
   * Reads package data from fileystem and sends it to npm-client
   */
   process(httpRequest, httpResponse) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       let packageName = httpRequest['params']['package'];
       let fileName = httpRequest['params']['filename'];
-      this.storage.getPackage(packageName, fileName).then((data) => {
+      this.storage.getPackage(packageName, fileName)
+        .catch((err) => reject(err))
+        .then((data) => {
         httpResponse.send(data);
         resolve();
+      }).catch((err) => {
+        httpResponse.send(err.toString());
       });
     });
   }
