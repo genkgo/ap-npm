67176850daa09019c3bbf2811e4c8c16f2681f24
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-06-30 14:43:33 +0200

    filled authmock

---
+++ test/mocks/authMock.js
@@ -0,0 +1,18 @@
+import path from 'path';
+
+export default class {
+  constructor() {
+  }
+
+  userLogin(username, password, email) {
+    return true;
+  }
+
+  userAdd(username, password, email) {
+    return true;
+  }
+
+  userRemove(username, password) {
+    return true;
+  }
+}
