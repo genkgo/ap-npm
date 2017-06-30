e0436536f9ce8351c38a33279f0878ef12ffb074
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-06-30 14:52:33 +0200

    added more mocks

---
+++ test/mocks/packageJsonMock.js
@@ -0,0 +1,61 @@
+export default {
+  "_id": "test-project",
+  "name": "test-project",
+  "description": "",
+  "dist-tags": {
+    "latest": "1.0.2"
+  },
+  "versions": {
+    "1.0.0": {
+      "name": "test-project",
+      "version": "1.0.0",
+      "description": "",
+      "main": "index.js",
+      "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+      },
+      "author": "",
+      "license": "ISC",
+      "dependencies": {
+        "@glimmer/blueprint": "^0.4.0"
+      },
+      "readme": "ERROR: No README data found!",
+      "_id": "test-project@1.0.0",
+      "_shasum": "bdb7190e8bfb8103f632f900d2a4a68db406d55c",
+      "_from": ".",
+      "_npmVersion": "3.10.10",
+      "_nodeVersion": "6.11.0",
+      "_npmUser": {},
+      "dist": {
+        "shasum": "bdb7190e8bfb8103f632f900d2a4a68db406d55c",
+        "tarball": "http://localhost:4444/test-project/-/test-project-1.0.0.tgz"
+      }
+    },
+    "1.0.2": {
+      "name": "test-project",
+      "version": "1.0.2",
+      "description": "",
+      "main": "index.js",
+      "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+      },
+      "author": "",
+      "license": "ISC",
+      "dependencies": {
+        "@glimmer/blueprint": "^0.4.0"
+      },
+      "readme": "ERROR: No README data found!",
+      "_id": "test-project@1.0.2",
+      "_shasum": "1d0d26206028c4a28d935a99e1a8aed7c35a97e1",
+      "_from": ".",
+      "_npmVersion": "3.10.10",
+      "_nodeVersion": "6.11.0",
+      "_npmUser": {},
+      "dist": {
+        "shasum": "1d0d26206028c4a28d935a99e1a8aed7c35a97e1",
+        "tarball": "http://localhost:4444/test-project/-/test-project-1.0.2.tgz"
+      }
+    }
+  },
+  "readme": "ERROR: No README data found!"
+}
