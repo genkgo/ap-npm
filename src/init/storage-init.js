c5bd8e80fb8a751bcdcbd4da9750f528004d8129
Mèir Noordermeer <m.noordermeer@genkgo.nl>
2017-07-06 13:52:14 +0200

    Add storage check to storage-init

--- src/init/storage-init.js
+++ src/init/storage-init.js
@@ -1,4 +1,18 @@
-export default function(container) {
+import path from 'path';
+import fse from 'fs-extra';
+
+export default function (container) {
+
+  let storageLocation = path.join(container.get('config').workDir, container.get('config').storage.directory);
+  try {
+    if (!fse.ensureDirSync(storageLocation)) {
+      fse.mkdirsSync(storageLocation);
+    }
+  } catch (err) {
+    container.get('logger').error("Failed to initialize filesystem-structure in " + storageLocation);
+  }
+
+
   container.set('storage-filesystem', function () {
     let Filesystem = require('../storage/filesystem/index').default;
     return new Filesystem(container.get('config'), container.get('logger'));
